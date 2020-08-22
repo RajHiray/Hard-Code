7b8f2a5d8cb159c065b319de564b3be1ac36c230
Raj Hiray <raj.hiray@outlook.com>
2020-08-22 12:26:11 +0530

    Add project files.

---
+++ TestAAD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestAAD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
